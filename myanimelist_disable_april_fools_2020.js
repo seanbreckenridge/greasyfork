9d3c40bc379e71ae0105aca4972e36e35cf74906
Sean Breckenridge <seanbrecke@gmail.com>
2020-04-02 22:04:18 -0700

    add april fools script and update install counts

---
+++ myanimelist_disable_april_fools_2020.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         MyAnimeList Disable April Fools 2020
+// @namespace    https://greasyfork.org/en/users/96096-purple-pinapples
+// @version      0.1.0
+// @description  Disables MALs emoji styling for April Fools 2020.
+// @author       PurplePinapples
+// @include      https://myanimelist.net/*
+// @license      WTFPL
+// @run-at       document-end
+// ==/UserScript==
+
+document.querySelector("body").classList.remove("aprilfools_2020")
