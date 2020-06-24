e86e8e156c87cf81c2b895851d8ad14a315e3c56
Sean Breckenridge <seanbrecke@gmail.com>
2020-06-24 14:39:48 -0700

    update autoconfirm script

--- myanimelist_autoconfirm_updated_entry.js
+++ myanimelist_autoconfirm_updated_entry.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MyAnimeList Auto-Confirm updated entry
 // @namespace    https://greasyfork.org/en/users/96096-purple-pinapples
-// @version      0.1.4
+// @version      0.2.0
 // @description  Automatically goes back to anime page after successfully updating an entry
 // @author       PurplePinapples
 // @license      WTFPL
@@ -16,6 +16,8 @@
 // @match        https://myanimelist.net/panel.php?go=addmanga&selected_manga_id=*
 // @match        https://myanimelist.net/ownlist/anime/add?selected_series_id=*
 // @match        https://myanimelist.net/ownlist/manga/add?selected_manga_id=*
+// @match        https://myanimelist.net/dbchanges.php?aid=*
+// @match        https://myanimelist.net/dbchanges.php?mid=*
 // @grant        none
 // ==/UserScript==
 
@@ -23,5 +25,9 @@
     'use strict';
     if ($("#content > .goodresult > strong:contains('Successfully')").length > 0) {
         location.href = $("#content > .goodresult > a:last-child").attr("href");
+    }
+    // structure for DB pages is slightly different
+    if (location.href.indexOf("dbchanges") != -1 && $(".goodresult:contains('Successfully')").length > 0) {
+        location.href = $(".goodresult:contains('Successfully') > a:last-child").attr("href");
     }
 })();
