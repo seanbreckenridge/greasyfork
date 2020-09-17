4171b8eefe76fa706155856e7f69faca7b5a46b1
Sean Breckenridge <seanbrecke@gmail.com>
2020-09-17 10:50:06 -0700

    update mangastore script

--- myanimelist_hide_manga_store.js
+++ myanimelist_hide_manga_store.js
@@ -1,15 +1,12 @@
 // ==UserScript==
 // @name         MyAnimeList Hide Manga Store
 // @namespace    https://greasyfork.org/en/users/96096-purplepinapples
-// @version      0.7
+// @version      0.8
 // @description  Hides the Manga Store Ads on MyAnimeList.
 // @author       PurplePinapples
 // @match        https://myanimelist.net/*
 // @license      MIT
 // ==/UserScript==
-function tail(some_list) {
-    return some_list[some_list.length - 1];
-}
 
 (function() {
     "use strict";
@@ -22,17 +19,7 @@ function tail(some_list) {
     $(".btn-forum-manga-store").hide();
     // if we're on the forum
     if (window.location.href.indexOf("topicid=") != -1) {
-        const manga_store_h2 = $("h2:contains('Manga Store')")
-        // bubble up tree till we hit something that contains a profile image (user profile), then hide the prev element
-        let elements = [manga_store_h2]
-        while ($(tail(elements)).find("a[href^='/profile']").length === 0) {
-            elements.push($(tail(elements).parent()))
-        }
-        // hide the second to last item
-        elements.pop() // remove highest item (full page which includes profile pic)
-        if (elements.length > 0) {
-            elements.pop().hide();
-        }
+        $("div > h2:contains('Manga Store')").parent().hide()
     } else {
         //on anime/manga page
         let c = window.location.href
