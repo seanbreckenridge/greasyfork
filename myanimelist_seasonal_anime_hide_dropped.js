fd87e9cb5bc05ded7843e83e3844c01e11d43709
Sean Breckenridge <seanbrecke@gmail.com>
2022-01-16 01:20:15 -0800

    update scripts

--- myanimelist_seasonal_anime_hide_dropped.js
+++ myanimelist_seasonal_anime_hide_dropped.js
@@ -1,16 +1,23 @@
 // ==UserScript==
 // @name         MyAnimeList Seasonal Anime Hide Dropped.
 // @namespace    https://greasyfork.org/en/users/96096-purple-pinapples
-// @version      0.1.2
-// @description  Toggles dropped to off on Seasonal Lists automatically
+// @version      0.1.3
+// @description  Hides Dropped Shows on the Seasonal Page
 // @author       PurplePinapples
-// @require      http://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
 // @license      WTFPL
 // @run-at       document-end
 // @include      https://myanimelist.net/anime/season*
 // ==/UserScript==
 
-var returnValue = function () {
+(() => {
     "use strict";
-    $("ul#mylist>li#dropped").removeClass("selected");
-}();
+    // hmm -- since the new update just removing the class doesnt work -- need to do this manually
+    document.querySelectorAll(".seasonal-anime").forEach((anime_card) => {
+      if(anime_card.querySelector(".btn-anime-watch-status.dropped")) {
+        // settings this to None doesnt even work, it resets after a few seconds
+        // anime_card.style.display = "none";
+        // think removing it is the easiest thing to do -- user'll have to toggle with their userscript runner if they want to view dropped
+        anime_card.outerHTML = '';
+      }
+    });
+})();
